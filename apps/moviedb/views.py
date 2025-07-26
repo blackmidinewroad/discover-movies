e18bbfb6be6a88d263ddd419adedd104cbbfccfa
blackmidinewroad <89681898397@bk.ru>
2025-07-27 00:21:36 +0300

    Add directors to movie detail template

--- apps/moviedb/views.py
+++ apps/moviedb/views.py
@@ -344,6 +344,7 @@ class MovieDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = f'{self.object.title}{f" - {self.object.release_date.year}" if self.object.release_date else ""}'
+        context['directors'] = self.object.directors.all()
         return context
 
 
---
+++ static/js/show_more_less.js
@@ -0,0 +1,18 @@
+function toggleDirectors() {
+    const list = document.querySelector('.directors-list');
+    const button = document.querySelector('.toggle-directors-btn');
+    const secondDirector = document.querySelectorAll('.director-link')[1] || document.querySelector('.directed-by');
+
+    const isHidden = list.style.display === 'none';
+
+    list.style.display = isHidden ? 'inline' : 'none';
+    button.classList.toggle('show-more', !isHidden);
+    button.classList.toggle('show-less', isHidden);
+    button.textContent = isHidden ? 'Show Less' : 'Show More';
+
+    if (isHidden) {
+        list.insertAdjacentElement('afterend', button);
+    } else {
+        secondDirector.insertAdjacentElement('afterend', button);
+    }
+}
