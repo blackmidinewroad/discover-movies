bb2ff50bba383e655363c94b830b2e41128f2957
blackmidinewroad <89681898397@bk.ru>
2025-08-24 23:11:42 +0300

    Add load_test_data.ps1

---
+++ load_test_data.ps1
@@ -0,0 +1,22 @@
+$startTime = Get-Date
+
+Write-Host "Loading countries"
+python manage.py update_countries
+Write-Host ""
+
+Write-Host "Loading languages"
+python manage.py update_languages
+Write-Host ""
+
+Write-Host "Loading genres"
+python manage.py update_genres
+Write-Host ""
+
+Write-Host "Loading 1000 most popular movies"
+python manage.py update_movies daily_export --batch_size 1000 --sort_by_popularity --limit 1000
+Write-Host ""
+
+$endTime = Get-Date
+$duration = $endTime - $startTime
+$rounded = [TimeSpan]::FromSeconds([Math]::Round($duration.TotalSeconds))
+Write-Host "Total Runtime: $($rounded.ToString())"
