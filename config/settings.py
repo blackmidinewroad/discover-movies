9b7efc2f7644f54586c852366dcbaab8fefbbbfc
blackmidinewroad <89681898397@bk.ru>
2025-09-07 14:04:42 +0300

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -186,8 +186,8 @@ INTERNAL_IPS = [
 ]
 
 CACHES = {
-    "default": {
-        "BACKEND": 'django.core.cache.backends.redis.RedisCache',
-        "LOCATION": 'redis://127.0.0.1:6379',
+    'default': {
+        'BACKEND': os.getenv('CACHES_BACKEND'),
+        'LOCATION': os.getenv('CACHES_LOCATION'),
     }
 }
