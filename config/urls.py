f575670461d6a3009f3b16fe4a7369047cc91f07
blackmidinewroad <89681898397@bk.ru>
2025-09-09 16:11:20 +0300

    Update admin url

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
+import os
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{os.getenv('ADMIN_PANEL_PATH', 'admin')}/', admin.site.urls),
     path('', include('apps.moviedb.urls')),
 ]
 
