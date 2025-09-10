46b8a400c949889758e786e999f113859e898156
blackmidinewroad <89681898397@bk.ru>
2025-09-10 14:50:15 +0300

    Prevent Celery from duplicating logs

--- config/celery.py
+++ config/celery.py
@@ -7,4 +7,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.update(worker_hijack_root_logger=False)  # prevent Celery from duplicating logs
 app.autodiscover_tasks()
